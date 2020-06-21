ee4aa64b38e7f708ac6ed9e6561b7a837b812977
HenryTufano <rique_3l@hotmail.com>
2020-06-21 14:14:29 -0400

    Atualização2

--- log.py
+++ log.py
@@ -2,77 +2,59 @@ import json
 import os
 users = {}
 costumers = {}
+pets = {}
 op = 99
-
-if os.path.isfile("users.json"): # caso já exista o arquivo
-    f = open("users.json", "r")
-    data = json.load(f)
-    f.close()
-    #print (data)
-    h = open("max_users.json", "r")
-    number_users = json.load(h)
-    h.close()
-    if number_users["number_users"] == "2":
-        print("Não é possível cadastrar mais do que 2 users")
-    else:
+while op != 0:
+    print("SISTEMA DE CONTROLE - PETIKÃO\n[1] - FAZER LOGIN\n[2] - CADASTRAR USUÁRIO\n[0] - FINALIZAR O PROGRAMA")
+    op = int(input("Escolha uma opção: "))
+    if op ==1:
         login = input("Digite o nome do usuário:  ")
         password = input("Digite a senha:  ")
-        users[login] = password
-        if login in data: # caso já exista um usuário com esse nome
-            print("usuário já cadastrado")
+        l =open("users.json", "r")
+        data =json.load(l)
+        if login in data :
+            if data[login] == password and data[password] == login:
+                print('deu certo')
+            else :
+                print('erro')
         else:
-            data[login] = password
-            f = open("users.json", "w")
-            json.dump(data, f)
+            print('login nao encontrado!')
+
+    elif op == 2:
+        if os.path.isfile("users.json"): # caso já exista o arquivo
+            f = open("users.json", "r")
+            data = json.load(f)
             f.close()
-            h = open("max_users.json","w")
-            number_users = {"number_users": "2"}
-            json.dump(number_users,h)
-            h.close
-else: # Criar arquivo, caso não exista
-    login = input("Digite o nome do usuário:  ")
-    password = input("Digite a senha:  ")
-    if os.path.isfile("users.json"):
-        users[login] = password
-        f = open("users.json","w") # w = write = escrita
-        json.dump(users, f)
-        f.close()
-        h = open("max_users.json","w")
-        number_users = {"number_users": "1"}
-        json.dump(number_users,h)
-        h.close
-
-#------------------------------------------------------------------------------------------------#
-
-login = input("Digite o nome do usuário:  ")
-password = input("Digite a senha:  ")
-#l =open("users.json", "r")
-#data =json.load(l)
-#user[login] = password
-
-if os.path.isfile("costumers.json"): # caso já exista o arquivo
-    j = open("costumers.json", "r")
-    datapet = json.load(j)
-    j.close()
-    #print (data)  
-    nome = input("Digite o nome do usuário:  ")
-    endereco = input("Digite o endereço:  ")
-    telefone = input("Digite o telefone:  ")
-    registered_by = login
-    if nome in datapet: # caso já exista um usuário com esse nome
-        print("Cliente já cadastrado")
-    else:
-        costumers[nome] = {'endereco': endereco,'fone' : telefone,'registrado' : registered_by}
-        j = open("costumers.json", "w")
-        json.dump(costumers, j)
-        j.close()
-else: # Criar arquivo, caso não exista
-    nome = input("Digite o nome do usuário:  ")
-    endereco = input("Digite o endereço:  ")
-    telefone = input("Digite o telefone:  ")
-    registered_by = users[login]
-    costumers[nome] = {'endereco': endereco,'fone' : telefone,'registrado' : registered_by}
-    j = open("costumers.json","w") # w = write = escrita
-    json.dump(costumers, j)
-    j.close()
-    
+            #print (data)
+            h = open("max_users.json", "r")
+            number_users = json.load(h)
+            h.close()
+            if number_users["number_users"] == "2":
+                print("Não é possível cadastrar mais do que 2 users")
+            else:
+                login = input("Digite o nome do usuário:  ")
+                password = input("Digite a senha:  ")
+                users[login] = password
+                if login in data: # caso já exista um usuário com esse nome
+                    print("usuário já cadastrado")
+                else:
+                    data[login] = password
+                    f = open("users.json", "w")
+                    json.dump(data, f)
+                    f.close()
+                    h = open("max_users.json","w")
+                    number_users = {"number_users": "2"}
+                    json.dump(number_users,h)
+                    h.close
+        else: # Criar arquivo, caso não exista
+            login = input("Digite o nome do usuário:  ")
+            password = input("Digite a senha:  ")
+            if os.path.isfile("users.json"):
+                users[login] = password
+                f = open("users.json","w") # w = write = escrita
+                json.dump(users, f)
+                f.close()
+                h = open("max_users.json","w")
+                number_users = {"number_users": "1"}
+                json.dump(number_users,h)
+                h.close
